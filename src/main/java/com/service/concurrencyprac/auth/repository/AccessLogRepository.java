974e70e84d8ba587d56ab1ed738317854d37fb66
lsc713 <lsc7134@naver.com>
2024-05-25 01:47:21 +0900

    feat: AccessLog 저장소 확정

--- src/main/java/com/service/concurrencyprac/auth/repository/AccessLogRepository.java
+++ src/main/java/com/service/concurrencyprac/auth/repository/AccessLogRepository.java
@@ -1,8 +1,8 @@
 package com.service.concurrencyprac.auth.repository;
 
-import com.service.concurrencyprac.auth.domain.AccessLogEntity;
+import com.service.concurrencyprac.auth.domain.token.AccessLog;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccessLogRepository extends JpaRepository<AccessLogEntity,Long> {
+public interface AccessLogRepository extends JpaRepository<AccessLog, Long> {
 
 }
