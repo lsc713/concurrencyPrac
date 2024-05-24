d73b3fae2f8d1563e84e0e11012978f87fa49b5b
lsc713 <lsc7134@naver.com>
2024-05-24 16:48:41 +0900

    fix: ErrorCode추가

--- src/main/java/com/service/concurrencyprac/common/response/ErrorCode.java
+++ src/main/java/com/service/concurrencyprac/common/response/ErrorCode.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum ErrorCode {
     COMMON_INVALID_PARAMETER("요청 값이 올바르지 않습니다."),
     COMMON_ENTITY_NOT_FOUND("존재하지 않는 엔티티입니다."),
-    OUT_OF_STOCK("재고가 부족합니다")
+    OUT_OF_STOCK("재고가 부족합니다"),
+    ILLEGAL_STATUS_FOR_PAYMENT("결제할 수 없는 상태입니다.")
     ;
 
     private final String errorMessage;
