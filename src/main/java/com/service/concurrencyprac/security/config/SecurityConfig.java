114be4a81887c85c21af3a96efcd08b9f4da6849
lsc713 <lsc7134@naver.com>
2024-05-25 01:50:10 +0900

    feat: security Filter Chain에 필터 추가

--- src/main/java/com/service/concurrencyprac/security/config/SecurityConfig.java
+++ src/main/java/com/service/concurrencyprac/security/config/SecurityConfig.java
@@ -1,26 +1,58 @@
 package com.service.concurrencyprac.security.config;
 
+import com.service.concurrencyprac.auth.jwt.JwtProvider;
+import com.service.concurrencyprac.auth.repository.AccessLogRepository;
+import com.service.concurrencyprac.security.service.JwtAuthenticationFilter;
+import com.service.concurrencyprac.security.service.JwtAuthorizationFilter;
+import com.service.concurrencyprac.security.service.UserDetailsServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtProvider jwtProvider;
+    private final UserDetailsServiceImpl userDetailsService;
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final AccessLogRepository accessLogRepository;
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() throws Exception {
+        JwtAuthenticationFilter filter = new JwtAuthenticationFilter(jwtProvider,
+            accessLogRepository);
+        filter.setAuthenticationManager(authenticationManager(authenticationConfiguration));
+        return filter;
+    }
+
+    @Bean
+    public JwtAuthorizationFilter jwtAuthorizationFilter() {
+        return new JwtAuthorizationFilter(jwtProvider, userDetailsService);
+    }
     @Bean
     public RoleHierarchy roleHierarchy() {
 
@@ -30,25 +62,31 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf((auth) -> auth.disable());
-        http
+    public SecurityFilterChain filterChain(HttpSecurity security) throws Exception {
+        security.csrf((auth) -> auth.disable());
+        security
             .formLogin((auth) -> auth.disable())
             .httpBasic(AbstractHttpConfigurer::disable);
 
-        http
+        security
             .sessionManagement(
                 (auth) -> auth.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
-        http
+        security
             .authorizeRequests((auth) -> auth
                 .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
                 .requestMatchers("/auth/**").permitAll()
                 .requestMatchers("/actuator/health").permitAll()
                 .requestMatchers("/manager").hasAnyRole("MANAGER")
                 .requestMatchers("/admin").hasAnyRole("ADMIN")
-                .requestMatchers("/my/**").hasAnyRole("ADMIN","MANAGER","USER")
+                .requestMatchers("/my/**").hasAnyRole("ADMIN", "MANAGER", "USER")
                 .anyRequest().authenticated());
-        return http.build();
+
+        security.addFilterBefore(jwtAuthorizationFilter(), JwtAuthenticationFilter.class);
+        security.addFilterBefore(jwtAuthenticationFilter(),
+            UsernamePasswordAuthenticationFilter.class);
+        return security.build();
+
+
     }
 }
