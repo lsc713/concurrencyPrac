ea49815515fa8467b339278d5f0624c93d7ee6ff
lsc713 <lsc7134@naver.com>
2024-05-27 01:36:15 +0900

    feat: 컨테이너 관리되도록 주석추가

--- src/main/java/com/service/concurrencyprac/auth/service/impl/AuthServiceImpl.java
+++ src/main/java/com/service/concurrencyprac/auth/service/impl/AuthServiceImpl.java
@@ -1,7 +1,9 @@
 package com.service.concurrencyprac.auth.service.impl;
 
 import com.service.concurrencyprac.auth.service.token.AuthService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AuthServiceImpl implements AuthService {
 
 }
--- src/main/java/com/service/concurrencyprac/auth/service/impl/TokenBlackListServiceImpl.java
+++ src/main/java/com/service/concurrencyprac/auth/service/impl/TokenBlackListServiceImpl.java
@@ -1,7 +1,9 @@
 package com.service.concurrencyprac.auth.service.impl;
 
 import com.service.concurrencyprac.auth.service.token.TokenBlackListService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TokenBlackListServiceImpl implements
     TokenBlackListService {
 
