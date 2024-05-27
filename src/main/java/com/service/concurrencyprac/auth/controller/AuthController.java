a52c3a103df3267022a97ae63e1fae913b23aafb
lsc713 <lsc7134@naver.com>
2024-05-27 17:03:04 +0900

    fix: logout추가

--- src/main/java/com/service/concurrencyprac/auth/controller/AuthController.java
+++ src/main/java/com/service/concurrencyprac/auth/controller/AuthController.java
@@ -2,9 +2,16 @@ package com.service.concurrencyprac.auth.controller;
 
 import com.service.concurrencyprac.auth.domain.member.MemberCommand.SignupMemberRequest;
 import com.service.concurrencyprac.auth.domain.member.MemberInfo;
+import com.service.concurrencyprac.auth.domain.token.TokenBlackList.TokenType;
 import com.service.concurrencyprac.auth.dto.MemberDTO;
+import com.service.concurrencyprac.auth.jwt.JwtProvider;
 import com.service.concurrencyprac.auth.service.impl.MemberServiceImpl;
+import com.service.concurrencyprac.auth.service.member.MemberService;
+import com.service.concurrencyprac.auth.service.token.AuthService;
+import com.service.concurrencyprac.auth.service.token.TokenBlackListService;
 import com.service.concurrencyprac.common.response.CommonResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,13 +25,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
-    private final MemberServiceImpl signupService;
+    private final JwtProvider jwtProvider;
+    private final MemberService signupService;
+    private final AuthService authService;
+    private final TokenBlackListService tokenBlackListService;
 
+    @Operation(summary = "회원가입")
     @PostMapping
     public CommonResponse signUp(@RequestBody @Valid MemberDTO.SignupRequest request) {
         SignupMemberRequest command = request.toCommand();
         MemberInfo response = signupService.signup(command);
         return CommonResponse.success(response);
+    }
+
+    @Operation(summary = "로그아웃")
+    @PostMapping("/logout")
+    public void logout(HttpServletRequest request) {
+        tokenBlackListService.addToBlacklist(
+            jwtProvider.getJwtFromHeader(request, TokenType.ACCESS),
+            jwtProvider.getJwtFromHeader(request, TokenType.REFRESH)
+        );
     }
 
     @GetMapping("/admin")
@@ -35,4 +55,5 @@ public class AuthController {
     public String login() {
         return "login";
     }
+
 }
--- src/main/java/com/service/concurrencyprac/auth/service/impl/AuthServiceImpl.java
+++ src/main/java/com/service/concurrencyprac/auth/service/impl/AuthServiceImpl.java
@@ -1,9 +1,11 @@
 package com.service.concurrencyprac.auth.service.impl;
 
 import com.service.concurrencyprac.auth.service.token.AuthService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
 
 }
