249ed16edeebb7dade4bbc4b44fd698f7939025f
lsc713 <lsc7134@naver.com>
2024-06-02 02:03:54 +0900

    feat: createOrderDto 일부 추가.

--- src/main/java/com/service/concurrencyprac/payment/dto/CreateOrderDto.java
+++ src/main/java/com/service/concurrencyprac/payment/dto/CreateOrderDto.java
@@ -5,15 +5,17 @@ import com.service.concurrencyprac.payment.entity.IssuedCoupon;
 import com.service.concurrencyprac.payment.entity.OrderItem;
 import com.service.concurrencyprac.payment.entity.ShippingInfo;
 import java.util.List;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class CreateOrderDto {
 
-    private Member userId;
-    private List<OrderItem> orderItems;
-    private IssuedCoupon couponId;
-    private Double pointAmountToUse;
-    private ShippingInfo shippingAddress;
+    private final Member userId;
+    private final List<OrderItem> orderItems;
+    private final IssuedCoupon couponId;
+    private final Double pointAmountToUse;
+    private final ShippingInfo shippingAddress;
 
 }
