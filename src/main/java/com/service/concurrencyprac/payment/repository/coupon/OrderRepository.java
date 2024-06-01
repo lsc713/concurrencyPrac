423bd24f1366fd741ee4529e841e28213eefa663
lsc713 <lsc7134@naver.com>
2024-06-02 02:44:30 +0900

    feat: OrderRepository EntityGraph

--- src/main/java/com/service/concurrencyprac/payment/repository/coupon/OrderRepository.java
+++ src/main/java/com/service/concurrencyprac/payment/repository/coupon/OrderRepository.java
@@ -1,8 +1,17 @@
 package com.service.concurrencyprac.payment.repository.coupon;
 
 import com.service.concurrencyprac.payment.entity.Order;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
+    // N+1 문제를 발생시키는 메서드
+    List<Order> findAll();
+
+    // N+1 문제를 해결하는 메서드
+    @EntityGraph(attributePaths = {"orderItems", "orderItems.product"})
+    List<Order> findAllWithOrderItemsBy();
 }
