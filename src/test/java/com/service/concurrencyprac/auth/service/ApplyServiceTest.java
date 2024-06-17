37befbd658d7ec39ab71eedd197f5bfb29ef92bc
lsc713 <lsc7134@naver.com>
2024-06-17 22:11:39 +0900

    feat: 테스트코드 주석처리

--- src/test/java/com/service/concurrencyprac/auth/service/ApplyServiceTest.java
+++ src/test/java/com/service/concurrencyprac/auth/service/ApplyServiceTest.java
@@ -1,86 +1,86 @@
-package com.service.concurrencyprac.auth.service;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
-import com.service.concurrencyprac.payment.repository.coupon.CouponRepository;
-import com.service.concurrencyprac.payment.service.impl.ApplyService;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ApplyServiceTest {
-
-    @Autowired
-    private ApplyService applyService;
-
-    @Autowired
-    private CouponRepository couponRepository;
-
-    @Test
-    public void 한번만응모() {
-        applyService.apply(1L);
-
-        long count = couponRepository.count();
-
-        assertThat(count).isEqualTo(1);
-    }
-
-    @Test
-    public void 여러명응모() throws Exception {
-        //given
-        int threadCount = 1000;
-        ExecutorService executorService = Executors.newFixedThreadPool(32);
-        CountDownLatch latch = new CountDownLatch(threadCount);
-
-        for (int i = 0; i < threadCount; i++) {
-            long userId = i;
-            executorService.submit(() -> {
-                try {
-                    applyService.apply(userId);
-
-                } finally {
-                    latch.countDown();
-                }
-            });
-        }
-        latch.await();
-
-        Thread.sleep(10000);
-
-        long count = couponRepository.count();
-
-        assertThat(count).isEqualTo(100);
-    }
-
-    @Test
-    public void 한명당_한개의쿠폰_발급() throws Exception {
-        //given
-        int threadCount = 1000;
-        ExecutorService executorService = Executors.newFixedThreadPool(32);
-        CountDownLatch latch = new CountDownLatch(threadCount);
-
-        for (int i = 0; i < threadCount; i++) {
-            long userId = i;
-            executorService.submit(() -> {
-                try {
-                    applyService.apply(1L);
-
-                } finally {
-                    latch.countDown();
-                }
-            });
-        }
-        latch.await();
-
-        Thread.sleep(10000);
-
-        long count = couponRepository.count();
-
-        assertThat(count).isEqualTo(1);
-    }
-
-}
+//package com.service.concurrencyprac.auth.service;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//import com.service.concurrencyprac.payment.repository.coupon.CouponRepository;
+//import com.service.concurrencyprac.payment.service.impl.ApplyService;
+//import java.util.concurrent.CountDownLatch;
+//import java.util.concurrent.ExecutorService;
+//import java.util.concurrent.Executors;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ApplyServiceTest {
+//
+//    @Autowired
+//    private ApplyService applyService;
+//
+//    @Autowired
+//    private CouponRepository couponRepository;
+//
+//    @Test
+//    public void 한번만응모() {
+//        applyService.apply(1L);
+//
+//        long count = couponRepository.count();
+//
+//        assertThat(count).isEqualTo(1);
+//    }
+//
+//    @Test
+//    public void 여러명응모() throws Exception {
+//        //given
+//        int threadCount = 1000;
+//        ExecutorService executorService = Executors.newFixedThreadPool(32);
+//        CountDownLatch latch = new CountDownLatch(threadCount);
+//
+//        for (int i = 0; i < threadCount; i++) {
+//            long userId = i;
+//            executorService.submit(() -> {
+//                try {
+//                    applyService.apply(userId);
+//
+//                } finally {
+//                    latch.countDown();
+//                }
+//            });
+//        }
+//        latch.await();
+//
+//        Thread.sleep(10000);
+//
+//        long count = couponRepository.count();
+//
+//        assertThat(count).isEqualTo(100);
+//    }
+//
+//    @Test
+//    public void 한명당_한개의쿠폰_발급() throws Exception {
+//        //given
+//        int threadCount = 1000;
+//        ExecutorService executorService = Executors.newFixedThreadPool(32);
+//        CountDownLatch latch = new CountDownLatch(threadCount);
+//
+//        for (int i = 0; i < threadCount; i++) {
+//            long userId = i;
+//            executorService.submit(() -> {
+//                try {
+//                    applyService.apply(1L);
+//
+//                } finally {
+//                    latch.countDown();
+//                }
+//            });
+//        }
+//        latch.await();
+//
+//        Thread.sleep(10000);
+//
+//        long count = couponRepository.count();
+//
+//        assertThat(count).isEqualTo(1);
+//    }
+//
+//}
--- src/test/java/com/service/concurrencyprac/payment/repository/coupon/OrderRepositoryPerformanceTest.java
+++ src/test/java/com/service/concurrencyprac/payment/repository/coupon/OrderRepositoryPerformanceTest.java
@@ -1,109 +1,110 @@
-package com.service.concurrencyprac.payment.repository.coupon;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-import com.service.concurrencyprac.auth.domain.member.Member;
-import com.service.concurrencyprac.auth.domain.member.Member.UserRole;
-import com.service.concurrencyprac.auth.repository.member.MemberRepository;
-import com.service.concurrencyprac.payment.entity.Order;
-import com.service.concurrencyprac.payment.entity.OrderItem;
-import com.service.concurrencyprac.payment.entity.Product;
-import java.util.ArrayList;
-import java.util.List;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
-
-//이안에 별도로 테스트코드생성용 레포 선언을해서 테스트해서 성능안좋은걸로 테스트를 위한 원래코드를 변경하는건 본말전도니..
-@SpringBootTest
-@Transactional
-public class OrderRepositoryPerformanceTest {
-
-    private static final Logger logger = LoggerFactory.getLogger(
-        OrderRepositoryPerformanceTest.class);
-
-    @Autowired
-    private OrderRepository orderRepository;
-
-    @Autowired
-    private OrderItemRepository orderItemRepository;
-
-    @Autowired
-    private ProductRepository productRepository;
-
-    @Autowired
-    private MemberRepository memberRepository;
-
-    private Member createMember() {
-        return Member.builder()
-            .email("test@example.com")
-            .password("password")
-            .name("Test Name")
-            .nickName("TestNickName")
-            .role(UserRole.USER)
-            .build();
-    }
-
-    private Product createProduct(String name, double price) {
-        return Product.builder()
-            .name(name)
-            .price(price)
-            .stock(10)
-            .build();
-    }
-
-    @AfterEach
-    public void tearDown() {
-        orderRepository.deleteAllInBatch();
-        orderItemRepository.deleteAllInBatch();
-        productRepository.deleteAllInBatch();
-        memberRepository.deleteAllInBatch();
-    }
-
-    @Test
-    public void testNPlusOneProblem() {
-        // 데이터 준비
-        Member member = createMember();
-        memberRepository.save(member);
-
-        for (int i = 0; i < 1000; i++) {
-            Product product = createProduct("Product" + i, 100.0);
-            productRepository.save(product);
-
-            OrderItem orderItem = OrderItem.builder()
-                .product(product)
-                .quantity(1)
-                .build();
-            orderItemRepository.save(orderItem);
-
-            Order order = Order.builder()
-                .member(member)
-                .orderItems(new ArrayList<>())
-                .build();
-            order.getOrderItems().add(orderItem);
-            orderRepository.save(order);
-        }
-
-        // N+1 문제 발생시키기
-        long startTime = System.currentTimeMillis();
-        List<Order> orders = orderRepository.findAll();
-        orders.forEach(order -> order.getOrderItems().size());
-        long durationNPlusOne = System.currentTimeMillis() - startTime;
-        logger.info("N+1 문제 발생 시간: {}ms", durationNPlusOne);
-
-        // N+1 문제 해결
-        startTime = System.currentTimeMillis();
-        List<Order> ordersWithItems = orderRepository.findAllWithOrderItems();
-        ordersWithItems.forEach(order -> order.getOrderItems().size());
-        long durationCached = System.currentTimeMillis() - startTime;
-        logger.info("캐싱된 제품 가져오기 시간: {}ms", durationCached);
-
-        // 성능 개선이 있는지 확인
-        assertTrue(durationCached < durationNPlusOne);
-    }
-}
+//package com.service.concurrencyprac.payment.repository.coupon;
+//
+//import static org.junit.jupiter.api.Assertions.assertTrue;
+//
+//import com.service.concurrencyprac.auth.domain.member.Member;
+//import com.service.concurrencyprac.auth.domain.member.Member.UserRole;
+//import com.service.concurrencyprac.auth.repository.member.MemberRepository;
+//import com.service.concurrencyprac.payment.entity.Order;
+//import com.service.concurrencyprac.payment.entity.OrderItem;
+//import com.service.concurrencyprac.payment.entity.Product;
+//import java.util.ArrayList;
+//import java.util.List;
+//import org.junit.jupiter.api.AfterEach;
+//import org.junit.jupiter.api.Test;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.transaction.annotation.Transactional;
+//
+//@SpringBootTest
+//@Transactional
+//public class OrderRepositoryPerformanceTest {
+//
+//    private static final Logger logger = LoggerFactory.getLogger(OrderRepositoryPerformanceTest.class);
+//
+//    @Autowired
+//    private OrderRepository orderRepository;
+//
+//    @Autowired
+//    private OrderItemRepository orderItemRepository;
+//
+//    @Autowired
+//    private ProductRepository productRepository;
+//
+//    @Autowired
+//    private MemberRepository memberRepository;
+//
+//    private Member createMember() {
+//        return Member.builder()
+//            .email("test@example.com")
+//            .password("password")
+//            .name("Test Name")
+//            .nickName("TestNickName")
+//            .role(UserRole.USER)
+//            .build();
+//    }
+//
+//    private Product createProduct(String name, double price) {
+//        return Product.builder()
+//            .name(name)
+//            .price(price)
+//            .stock(10)
+//            .build();
+//    }
+//
+//    @AfterEach
+//    public void tearDown() {
+//        orderItemRepository.deleteAllInBatch();
+//        orderRepository.deleteAllInBatch();
+//        productRepository.deleteAllInBatch();
+//        memberRepository.deleteAllInBatch();
+//    }
+//
+//    @Test
+//    public void testNPlusOneProblem() {
+//        // 데이터 준비
+//        Member member = createMember();
+//        memberRepository.save(member);
+//
+//        for (int i = 0; i < 100; i++) {
+//            Product product = createProduct("Product" + i, 100.0);
+//            productRepository.save(product);
+//
+//            OrderItem orderItem = OrderItem.builder()
+//                .product(product)
+//                .quantity(1)
+//                .build();
+//            orderItemRepository.save(orderItem);
+//
+//            Order order = Order.builder()
+//                .member(member)
+//                .orderItems(new ArrayList<>())
+//                .build();
+//            order.addOrderItem(orderItem);
+//            orderRepository.save(order);
+//        }
+//
+//        // Lazy Loading 방식 시간 측정
+//        long totalDurationLazyLoading = 0;
+//            long startTime = System.currentTimeMillis();
+//            List<Order> orders = orderRepository.findAll();
+//            orders.forEach(order -> order.getOrderItems().forEach(OrderItem::getProduct));
+//            totalDurationLazyLoading += (System.currentTimeMillis() - startTime);
+//        long durationLazyLoading = totalDurationLazyLoading;
+//        logger.info("Lazy Loading 방식 평균 시간: {}ms", durationLazyLoading);
+//
+//        // @EntityGraph 방식 시간 측정
+//        long totalDurationEntityGraph = 0;
+//            long startEGTime = System.currentTimeMillis();
+//            List<Order> ordersWithItems = orderRepository.findAllWithOrderItemsBy();
+//            ordersWithItems.forEach(order -> order.getOrderItems().forEach(OrderItem::getProduct));
+//            totalDurationEntityGraph += (System.currentTimeMillis() - startEGTime);
+//        long durationEntityGraph = totalDurationEntityGraph;
+//        logger.info("@EntityGraph 방식 평균 시간: {}ms", durationEntityGraph);
+//
+//        // 성능 개선이 있는지 확인
+//        assertTrue(durationEntityGraph < durationLazyLoading);
+//    }
+//}
