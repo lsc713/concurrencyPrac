df732f11c850f004d51a4b8148d59a0dbcd197ef
lsc713 <lsc7134@naver.com>
2024-06-19 23:27:44 +0900

    fix: eslint-disable(warning ignoring) + navbar + list 추가

--- src/main/concurrency/src/App.js
+++ src/main/concurrency/src/App.js
@@ -8,6 +8,7 @@ import axios from "axios";
 function App() {
   const [hello, setHello] = useState('');
   let [title, settitle] =useState(['1','2','3'])
+  let [like, changeLike] = useState(0);
 
   useEffect(() => {
     axios.get('http://localhost:8080/api/v1/auth/signup')
@@ -24,16 +25,25 @@ function App() {
         <div className="black-nav">
           <h4>상단바</h4>
         </div>
+        <button onClick={()=>{
+          let copy = [...title];
+          copy.sort()
+          settitle(copy)
+        }}>정렬</button>
         <div className="list">
-          <h4>{title[0]}</h4>
+          <h4 onClick={()=>{
+            let copy = [...title];
+            copy[0] = '추천게시물';
+            settitle(copy);
+          }}>{title[0]} <span onClick={()=>{changeLike(like+1)}}>👍</span>{like}</h4>
           <p>content</p>
         </div>
         <div className="list">
-          <h4>{title[0]}</h4>
+          <h4>{title[1]}</h4>
           <p>content</p>
         </div>
         <div className="list">
-          <h4>{title[0]}</h4>
+          <h4>{title[2]}</h4>
           <p>content</p>
         </div>
         백엔드 데이터 : {hello}
